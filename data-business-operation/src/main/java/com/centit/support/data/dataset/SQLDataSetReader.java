da7c7613b78429f2058ef010420d1bd5b7953ebb
杨淮生 <codefan@centit.com>
2019-01-30 17:00:47 +0800

    完成数据库读取和写入操作

--- data-business-operation/src/main/java/com/centit/support/data/dataset/SQLDataSetReader.java
+++ data-business-operation/src/main/java/com/centit/support/data/dataset/SQLDataSetReader.java
@@ -1,9 +1,19 @@
 package com.centit.support.data.dataset;
 
+import com.alibaba.fastjson.JSONArray;
 import com.centit.support.data.core.DataSet;
 import com.centit.support.data.core.DataSetReader;
-import com.centit.support.data.core.DataSetWriter;
+import com.centit.support.data.core.SimpleDataSet;
+import com.centit.support.database.utils.DataSourceDescription;
+import com.centit.support.database.utils.DatabaseAccess;
+import com.centit.support.database.utils.DbcpConnectPools;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,15 +24,50 @@ import java.util.Map;
  */
 public class SQLDataSetReader implements DataSetReader {
 
+    private static final Logger logger = LoggerFactory.getLogger(SQLDataSetReader.class);
+    private DataSourceDescription dataSource;
 
+    private String sqlSen;
+
+    private Connection connection;
     /**
      * 读取 dataSet 数据集
      * @param params 模块的自定义参数
      * @return dataSet 数据集
      */
     @Override
+    @SuppressWarnings("unchecked")
     public DataSet load(Map<String, Object> params) {
+        Connection conn = connection;
+        boolean createConnect = false;
+        try {
+            if(conn == null){
+                conn = DbcpConnectPools.getDbcpConnect(dataSource);
+                createConnect = true;
+            }
+            JSONArray jsonArray = DatabaseAccess.findObjectsByNamedSqlAsJSON(
+                conn, sqlSen, params);
+            SimpleDataSet dataSet = new SimpleDataSet();
+            dataSet.setData((List)jsonArray);
+            return dataSet;
+        } catch (SQLException | IOException e) {
+            logger.error(e.getLocalizedMessage());
+        }
+        if(createConnect){
+            DbcpConnectPools.closeConnect(conn);
+        }
         return null;
     }
 
+    public void setDataSource(DataSourceDescription dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void setSqlSen(String sqlSen) {
+        this.sqlSen = sqlSen;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
 }
