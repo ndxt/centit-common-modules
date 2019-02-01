32d9024a4de977f2995e6923267cfed80aa71f89
zou_wy <zou_wy@centit.com>
2019-02-01 10:38:00 +0800

    group by

--- data-business-operation/src/main/java/com/centit/support/data/utils/DataSetOptUtil.java
+++ data-business-operation/src/main/java/com/centit/support/data/utils/DataSetOptUtil.java
@@ -7,7 +7,9 @@ import com.centit.support.algorithm.NumberBaseOpt;
 import com.centit.support.algorithm.StringBaseOpt;
 import com.centit.support.data.core.DataSet;
 import com.centit.support.data.core.SimpleDataSet;
+import org.apache.commons.collections4.ListUtils;
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import java.util.*;
@@ -22,7 +24,8 @@ public abstract class DataSetOptUtil {
         if (data == null || data.size() == 0) {
             return inData;
         }
-        //TODO 按group by字段排序
+        //按group by字段排序
+        sortByFields(data, groupbyFields);
 
         List<Map<String, Object>> newData = new ArrayList<>();
         Map<String, Object> newRow = new LinkedHashMap<>();
@@ -105,8 +108,8 @@ public abstract class DataSetOptUtil {
         if (rowHeaderFields.size() + colHeaderFields.size() + 1 != data.get(0).size()) {
             throw new RuntimeException("数据不合法");
         }
-        // TODO 根据维度进行排序 行头、列头
-
+        //根据维度进行排序 行头、列头
+        sortByFields(data, ListUtils.union(rowHeaderFields, colHeaderFields));
 
         List<Map<String, Object>> newData = new ArrayList<>();
 
@@ -174,30 +177,40 @@ public abstract class DataSetOptUtil {
         return inData;
     }
 
-    public static DataSet compareTabulation(DataSet currData, DataSet lastData) {
+    public static DataSet compareTabulation(DataSet currData, DataSet lastData, List<String> primaryFields) {
         // 这个函数需要传入 主键列
         // 需要根据主键排序
         return currData;
     }
 
-    private static void sortByField(List<Map<String, Object>> data, List<String> fields) {
+    private static void sortByFields(List<Map<String, Object>> data, List<String> fields) {
 
-        for(Map<String, Object> row : data){
+        Collections.sort(data, (o1, o2) -> {
+            int[] sort = new int[fields.size()];
+            int i = 0;
+            for(String field : fields){
+                sort[i++] = StringUtils.compare(StringBaseOpt.castObjectToString(o1.get(field)), StringBaseOpt.castObjectToString(o2.get(field)));
+            }
+            for(int j = 0; j < sort.length; j++){
+                if(sort[j] != 0){
+                    return sort[j];
+                }
+            }
+            return 0;
+        });
 
-        }
     }
 
     public static void main(String[] args) {
         List<Map<String, Object>> list = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("century", "21");
-            map.put("year", "2018");
-            map.put("season", i + 1);
-            map.put("month", (i + 1) * 3);
-            map.put("seal", 10 + i);
-            list.add(map);
-        }
+
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("century", "21");
+        map1.put("year", "2019");
+        map1.put("season", 4);
+        map1.put("month", 11);
+        map1.put("seal", 36);
+        list.add(map1);
         for (int i = 0; i < 3; i++) {
             Map<String, Object> map = new HashMap<>();
             map.put("century", "21");
@@ -205,6 +218,15 @@ public abstract class DataSetOptUtil {
             map.put("season", i + 1);
             map.put("month", (i + 1) * 3);
             map.put("seal", 30 + i);
+            list.add(map);
+        }
+        for (int i = 0; i < 4; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("century", "21");
+            map.put("year", "2018");
+            map.put("season", i + 1);
+            map.put("month", (i + 1) * 3);
+            map.put("seal", 10 + i);
             list.add(map);
         }
 
@@ -236,6 +258,14 @@ public abstract class DataSetOptUtil {
             map.put("season", 2);
             map.put("income", 2+i);
             map.put("seal", 1 + i);
+            list1.add(map);
+        }
+        for (int i = 0; i < 4; i++) {
+            Map<String, Object> map = new LinkedHashMap<>();
+            map.put("year", "2018");
+            map.put("season", 1);
+            map.put("income", 1+i);
+            map.put("seal", i);
             list1.add(map);
         }
         List<String> groupByFields = new ArrayList<>();
