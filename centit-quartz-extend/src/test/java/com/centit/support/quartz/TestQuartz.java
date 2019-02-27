89d9d606f674745def1ac362d9e2b6d847d07ab0
杨淮生 <codefan@centit.com>
2019-02-27 16:52:26 +0800

    添加 日志记录，并且编制说明

--- centit-quartz-extend/src/test/java/com/centit/support/quartz/TestQuartz.java
+++ centit-quartz-extend/src/test/java/com/centit/support/quartz/TestQuartz.java
@@ -1,5 +1,7 @@
 package com.centit.support.quartz;
 
+import com.centit.framework.components.OperationLogCenter;
+import com.centit.framework.components.impl.TextOperationLogWriterImpl;
 import com.centit.support.algorithm.CollectionsOpt;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
@@ -11,6 +13,10 @@ import java.util.concurrent.TimeUnit;
 public class TestQuartz {
 
     public static void main(String[] args) throws SchedulerException, InterruptedException {
+        TextOperationLogWriterImpl textOperationLogWriter
+            = new TextOperationLogWriterImpl();
+        textOperationLogWriter.setOptLogHomePath("/D/Projects/RunData/demo_home/logs");
+        OperationLogCenter.initOperationLogWriter(textOperationLogWriter);
         /** 创建 schedulerFactory
          *  在Spring 框架中无需手动创建引入 对应的配置文件就可以，并通过下面代码获取
          *  WebApplicationContext webApplicationContext = ContextLoader.getCurrentWebApplicationContext();
@@ -23,7 +29,7 @@ public class TestQuartz {
          * 注册 job的类型，可以自行扩展 job类别
          */
         QuartzJobUtils.registerJobType("msg", PrintMessageJob.class);
-        QuartzJobUtils.registerJobType("httpRequest", HttpRquestJob.class);
+        //QuartzJobUtils.registerJobType("httpRequest", HttpRquestJob.class);
         QuartzJobUtils.registerJobType("exec", CallProcessJob.class);
 
         QuartzJobUtils.createOrReplaceSimpleJob(scheduler, "print",
@@ -35,7 +41,7 @@ public class TestQuartz {
         QuartzJobUtils.createOrReplaceSimpleJob(scheduler, "print",
             "test", "msg", 4,
             CollectionsOpt.createHashMap("message", "Hellow quartz!"));
-        TimeUnit.SECONDS.sleep(10);
+        TimeUnit.SECONDS.sleep(40);
         scheduler.shutdown();
         System.out.println("--------scheduler shutdown ! ------------");
     }
