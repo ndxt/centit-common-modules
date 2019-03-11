02073661f552acdc25aef92a3d9fb61f14b29d8d
chenlian <chen_l@centit.com>
2019-03-11 17:20:07 +0800

    DataSetOptUtil.analyseDataset单元测试

--- data-business-operation/src/test/java/com/centit/support/test/TestAnalyseDataset.java
+++ data-business-operation/src/test/java/com/centit/support/test/TestAnalyseDataset.java
@@ -6,8 +6,6 @@ import com.centit.support.dataopt.utils.DataSetOptUtil;
 import junit.framework.TestCase;
 import org.apache.commons.lang3.tuple.MutablePair;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.commons.lang3.tuple.Triple;
-import org.apache.commons.lang3.tuple.MutableTriple;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -21,35 +19,38 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * DataSetOptUtil.statDataset单元测试
+ * DataSetOptUtil.analyseDataset单元测试
  *
  * @author chen_l
- * @date 2019/3/8
+ * @date 2019/3/11
  */
-public class TestStatDataset extends TestCase {
+public class TestAnalyseDataset extends TestCase {
     //打印日志
     Logger logger = LoggerFactory.getLogger(TestCrossTabulation.class);
     //测试excel存放路径
-    private final String path = Class.class.getClass().getResource("/").getPath() + "com\\centit\\support\\test\\TestStatDataset.xlsx";
+    private final String path = Class.class.getClass().getResource("/").getPath() + "com\\centit\\support\\test\\TestAnalyseDataset.xlsx";
     //输入数据集的数据存放sheet页名称
     private final String initDataSheetName = "initData";
     //分组（排序）字段、统计字段的数据存放sheet页名称
-    private final String groupFieldsSheetName = "GroupFields";
+    private final String fieldsSheetName = "Fields";
     // 预期结果数据集的数据存放sheet页名称
     private final String resultSheetName = "result";
-    //分组（排序）字段数据的行头名称
-    private final String groupFieldsName = "groupFields";
-    //统计字段数据的行头名称
-    private final String statDescName = "statDesc";
+    //分组字段数据的行头名称
+    private final String groupFieldsName = "groupbyFields";
+    //排序字段数据的行头名称
+    private final String orderbyFieldsName = "orderbyFields";
+    //引用说明数据的行头名称
+    private final String refDescName = "refDesc";
     //输入数据集的数据集合
     private List<Map<String, Object>> initDataMapList = new ArrayList<Map<String, Object>>();
-    //分组（排序）字段的数据集合
+    //分组字段的数据集合
     private List<String> groupFields = new ArrayList<String>();
-    //统计字段的数据集合
-    private List<Triple<String, String, String>> statDescList = new ArrayList<Triple<String, String, String>>();
+    //排序字段的数据集合
+    private List<String> orderbyFields = new ArrayList<String>();
+    //引用说明的数据集合
+    private List<Pair<String, String>> refDescList = new ArrayList<Pair<String, String>>();
     //预期结果数据集的数据集合
     private List<Map<String, Object>> resultMapList = new ArrayList<Map<String, Object>>();
-
 
     @Before
     public void setUp() throws Exception {
@@ -58,23 +59,24 @@ public class TestStatDataset extends TestCase {
         ReadExcel readExcel = new ReadExcel();
         dataMap = readExcel.getDataMap(path);
         initDataMapList = readExcel.getDataMapList(dataMap.get(initDataSheetName), true);
-        Map<String, ArrayList<String>> fieldsMap = readExcel.getFieldsMap(dataMap.get(groupFieldsSheetName));
+        Map<String, ArrayList<String>> fieldsMap = readExcel.getFieldsMap(dataMap.get(fieldsSheetName));
         groupFields = fieldsMap.get(groupFieldsName);
-        for (String desc : fieldsMap.get(statDescName)) {
+        orderbyFields = fieldsMap.get(orderbyFieldsName);
+        for (String desc : fieldsMap.get(refDescName)) {
             String[] descArray = desc.split(":");
-            statDescList.add(new MutableTriple<String, String, String>(descArray[0], descArray[1], descArray[2]));
+            refDescList.add(new MutablePair<String, String>(descArray[0], descArray[1]));
         }
         resultMapList = readExcel.getDataMapList(dataMap.get(resultSheetName), true);
     }
 
     @Test
     public void testStatDataset() {
-        DataSet resultSetActual = DataSetOptUtil.statDataset(new SimpleDataSet(initDataMapList), groupFields, statDescList);
+        DataSet resultSetActual = DataSetOptUtil.analyseDataset(new SimpleDataSet(initDataMapList), groupFields, orderbyFields, refDescList);
         System.out.println("预期结果：" + resultMapList);
         System.out.println("实际结果：" + resultSetActual.getData());
         Assert.assertEquals(resultMapList, resultSetActual.getData());
-        System.out.println("方法statDataset测试通过！");
-        logger.info("方法statDataset测试通过！");
+        System.out.println("方法analyseDataset测试通过！");
+        logger.info("方法analyseDataset测试通过！");
     }
 
     @After
