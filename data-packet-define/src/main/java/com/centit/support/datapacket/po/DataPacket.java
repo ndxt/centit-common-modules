fa0a59fbf062ace9b947b9876b339ffd30ae6147
杨淮生 <codefan@centit.com>
2019-03-12 15:49:41 +0800

    重构代码

--- data-packet-define/src/main/java/com/centit/support/datapacket/po/DataPacket.java
+++ data-packet-define/src/main/java/com/centit/support/datapacket/po/DataPacket.java
@@ -1,11 +1,23 @@
 package com.centit.support.datapacket.po;
 
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
+import com.centit.framework.core.dao.DictionaryMap;
+import com.centit.support.database.orm.GeneratorCondition;
+import com.centit.support.database.orm.GeneratorTime;
+import com.centit.support.database.orm.GeneratorType;
+import com.centit.support.database.orm.ValueGenerator;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -14,23 +26,57 @@ import java.util.Map;
 @Entity
 public class DataPacket {
 
+    @ApiModelProperty(value = "数据包ID", hidden = true)
+    @Id
+    @Column(name = "PACKET_ID")
+    @NotBlank(message = "字段不能为空")
     private String packetId;
 
-    private String packetName;
+    @ApiModelProperty(value = "数据包名称模板")
+    @Column(name = "PACKET_NAME_FORMAT")
+    @NotBlank(message = "字段不能为空")
+    private String packetNameFormat;
     /**
-     * 数据包类别，主要有 D database， F file ， P directory 文件夹
+     * 数据包类别，主要有 D database， F file ， P directory 文件夹 , 默认值为D
      */
+    @ApiModelProperty(value = "数据包类别，主要有 D database, F file, P directory 文件夹, 默认值为D")
+    @Column(name = "PACKET_TYPE")
+    @NotBlank(message = "字段不能为空")
     private String packetType;
 
     /**
      * 数据包参数： 查询参数描述
      */
+    @ApiModelProperty(value = "数据包参数： 查询参数描述")
+    @Column(name = "PACKET_PARAMS_JSON")
     private String packetParamsJSON;
     /**
      * 详细描述
      */
+    @ApiModelProperty(value = "详细描述")
+    @Column(name = "PACKET_DESC")
     private String packetDesc;
 
+    @Column(name = "OWNER_TYPE")
+    @ApiModelProperty(value = "属主类别（D:部门；U:用户）")
+    private String ownerType;
+
+    @Column(name = "OWNER_CODE")
+    @ApiModelProperty(value = "属主代码")
+    private String ownerCode;
+
+    @Column(name = "RECORDER")
+    @ApiModelProperty(value = "修改人", hidden = true)
+    @DictionaryMap(fieldName = "recorderName", value = "userCode")
+    private String recorder;
+
+    @Column(name = "RECORD_DATE")
+    @ApiModelProperty(value = "修改时间", hidden = true)
+    @ValueGenerator(strategy = GeneratorType.FUNCTION, occasion = GeneratorTime.NEW_UPDATE, condition = GeneratorCondition.ALWAYS, value = "today()")
+    @JSONField(serialize = false)
+    private Date recordDate;
+
+    @Transient
     private Map<String, Object> packetParams;
 
     public DataPacket(){
--- data-packet-define/src/main/java/com/centit/support/datapacket/po/RmdbDataQuery.java
+++ data-packet-define/src/main/java/com/centit/support/datapacket/po/RmdbDataQuery.java
@@ -1,11 +1,15 @@
 package com.centit.support.datapacket.po;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Transient;
+import javax.validation.constraints.NotBlank;
 import java.util.Map;
 
 @ApiModel
@@ -14,26 +18,41 @@ import java.util.Map;
 @Entity
 public class RmdbDataQuery {
 
+    @ApiModelProperty(value = "数据包ID", hidden = true)
+    @Id
+    @Column(name = "QUERY_ID")
+    @NotBlank(message = "字段不能为空")
     private String queryId;
 
+    @ApiModelProperty(value = "数据包ID", hidden = true)
+    @Column(name = "PACKET_ID")
+    @NotBlank(message = "字段不能为空")
     private String packetId;
 
+    @ApiModelProperty(value = "查询名称")
+    @Column(name = "QUERY_NAME")
     private String queryName;
 
+    @ApiModelProperty(value = "查询描述")
+    @Column(name = "QUERY_DESC")
     private String queryDesc;
 
-    private String databaseId;
+    @Column(name = "DATABASE_CODE")
+    @ApiModelProperty(value = "数据库代码，引用集成平台中定义的数据库")
+    private String databaseCode;
 
+
+    @ApiModelProperty(value = "带命名参数的sql语句")
+    @Column(name = "QUERY_SQL")
     private String querySQL;
 
     /**
      * 字段名 描述
      */
+    @ApiModelProperty(value = "sql语句字段名定义信息，是一个json格式的字符串")
+    @Column(name = "FIELD_NAMES_JSON")
     private String fieldNamesJSON;
-    /**
-     * 详细描述
-     */
-    private String packetDesc;
+
 
     @Transient
     private Map<String, String> fieldNames;
