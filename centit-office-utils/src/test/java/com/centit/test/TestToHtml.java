b8af11d0b62dbabfb23561658a612a7b57af61d0
codefan <codefan@sina.com>
2020-10-15 10:54:21 +0800

    xlsx转pdf 报错

--- centit-office-utils/src/test/java/com/centit/test/TestToHtml.java
+++ centit-office-utils/src/test/java/com/centit/test/TestToHtml.java
@@ -5,10 +5,10 @@ import com.centit.support.office.OfficeToHtml;
 public class TestToHtml {
 
     public static void main(String[] args) {
-        OfficeToHtml.word2Html("/D/Projects/RunData/demo_home/temp/case6.docx",
-            "/D/Projects/RunData/demo_home/temp/case6.html");
-        OfficeToHtml.word2Html("/D/Projects/RunData/demo_home/temp/case7.doc",
-            "/D/Projects/RunData/demo_home/temp/case7.html");
+        OfficeToHtml.excel2Html("/D/Projects/RunData/file_home/六上课程表.xlsx",
+            "/D/Projects/RunData/file_home/六上课程表.html");
+        OfficeToHtml.excel2Html("/D/Projects/RunData/file_home/centit-info.xlsx",
+            "/D/Projects/RunData/file_home/centit-info.html");
         //POIPptToHtmlUtils.pptToHtml("D:/diagnosis/file/temp//ppt2007.pptx", "D:/diagnosis/file/temp/", "test5.html");
        // OfficeToHtml.ppt2Html("C:\\Users\\zhf\\Postman\\files\\2.pptx", "C:\\Users\\zhf\\Postman\\files\\tmp\\2.html","pptx");
     }
